d0056c9e4dd3202efce95a7cfc7daec93ee82f31
dima-bzz <dimabzz@gmail.com>
2018-02-08 15:31:34 +0300

    Small fix

--- functions.inc.php
+++ functions.inc.php
@@ -69,7 +69,7 @@ if ($forhostname == "/") {$CONF['hostname']=$CONF['hostname'];}
 else if ($forhostname <> "/") {$CONF['hostname']=$CONF['hostname']."/";}
 
 function get_version(){
-  $v = '2.21.1';
+  $v = '2.21.2';
   return $v;
 }
 
